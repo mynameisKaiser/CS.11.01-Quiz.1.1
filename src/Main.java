6d274c2cd1b69fba0e6531df3e377cd0bc45913a
mynameisKaiser <kaisertsoi@hotmail.com>
2024-08-27 14:17:22 +0800

    Submittion

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,26 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Kaiser
 *****************************************************************************************************
 */
 
 public class Main {
-
     public static void main(String[] args) {
-
-
-
+    int age = 0;
+    String firstName="";
+    String favouriteFood="";
+    System.out.println("How old are you? ");
+    Scanner ageScan = new Scanner(System.in);
+    String ageStr = ageScan.nextLine();
+    age = Integer.parseInt(ageStr);
+    System.out.println("What is your name? ");
+    Scanner name=new Scanner(System.in);
+    firstName=name.nextLine();
+    System.out.println("What is your favourite food? ");
+    Scanner food=new Scanner(System.in);
+    favouriteFood=name.nextLine();
+    System.out.println("Name: "+firstName+"\n"+"Age: "+age+"\n"+"Favourite food: "+favouriteFood);
 
     }
 
